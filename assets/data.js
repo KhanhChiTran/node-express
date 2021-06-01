655ef7c80aa0ac72c68a3667e7f14d4504abe795
Chi Tran <khanhchi.tran13051@gmail.com>
2021-06-01 18:09:09 +0200

    hello video stream

---
+++ assets/data.js
@@ -0,0 +1,28 @@
+module.exports = {
+  records: [
+    {
+      title: "I am easy to find",
+      artist: "The National",
+      year: 2019,
+      img: "I am a url",
+      price: 40,
+      id: "1568387157969",
+    },
+    {
+      title: "I am easy to find",
+      artist: "The National",
+      year: 2019,
+      img: "I am a url",
+      price: 40,
+      id: "1568387159950",
+    },
+    {
+      title: "Something eeeeelse",
+      artist: "The National",
+      year: 2019,
+      img: "I am a url",
+      price: 40,
+      id: "1568389444500",
+    },
+  ],
+}
--- index.js
+++ index.js
@@ -1,10 +1,33 @@
 const express = require("express")
+const fs = require("fs")
 const path = require("path")
+
+const data = require("./assets/data")
 
 const app = express()
 
 app.get("/", (req, res) => {
   res.send("Hello World")
+})
+app.get("/api/records", (req, res) => {
+  res.send(data)
+})
+
+app.get("/video", (req, res) => {
+  const videoPath = path.join(__dirname, "assets", "sample.mp4")
+  const stat = fs.statSync(videoPath)
+  const fileSize = stat.size
+  console.log(req.headers.range)
+  // res.send("video is playing ...")
+  const head = {
+    // "Content-Length": fileSize,
+    "Content-Type": "video/mp4",
+  }
+  const streamVideo = fs.createReadStream(videoPath)
+  res.writeHead(200, head)
+  streamVideo.on("data", chunk => res.write(chunk))
+  streamVideo.on("end", () => res.end())
+  // fs.createReadStream(videoPath).pipe(res)
 })
 
 const PORT = 3000
@@ -12,7 +35,5 @@ const PORT = 3000
 app.use(express.static(path.join(__dirname, "assets", "images")))
 
 app.listen(PORT, () => {
-  console.log("App is running in port ", PORT)
+  console.log("App is running on port ", PORT)
 })
-
-console.log(__dirname)
